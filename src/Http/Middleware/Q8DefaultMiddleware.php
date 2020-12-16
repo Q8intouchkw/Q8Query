2adcc06990a1b72a2e3eb2fa862f396d8519b839
k-sae <karim.salah2048@gmail.com>
2020-12-16 18:20:48 +0200

    fix a closure issue

--- src/Http/Middleware/Q8DefaultMiddleware.php
+++ src/Http/Middleware/Q8DefaultMiddleware.php
@@ -2,13 +2,15 @@
 namespace Q8Intouch\Q8Query\Http\Middleware;
 
 
+use Closure;
+
 class Q8DefaultMiddleware
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
