edffa6796d82265787e10d3b41f2abbbdd04d592
k-sae <karim.salah2048@gmail.com>
2020-12-16 18:14:31 +0200

    add a default middleware incase of null

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Q8Intouch\Q8Query\Http\Controllers\QueryController;
-Route::middleware(config('q8-query.middleware'))->group(function (){
+Route::middleware(config('q8-query.middleware') ?? \Q8Intouch\Q8Query\Http\Middleware\Q8DefaultMiddleware::class)->group(function (){
     Route::get('/{resource}', QueryController::class.'@get')
         ->where('resource', '.*');
 
---
+++ src/Http/Middleware/Q8DefaultMiddleware.php
@@ -0,0 +1,18 @@
+<?php
+namespace Q8Intouch\Q8Query\Http\Middleware;
+
+
+class Q8DefaultMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+    }
+}
