e698f3cf32f60f7e2783c94ddcf88c0a43190fd5
k-sae <karim.salah2048@gmail.com>
2020-12-27 12:02:15 +0200

    code cleanup

--- src/Filterer/FilterMethods/DoesntHaveFilterer.php
+++ src/Filterer/FilterMethods/DoesntHaveFilterer.php
@@ -3,17 +3,12 @@
 namespace Q8Intouch\Q8Query\Filterer\FilterMethods;
 
 
-use Illuminate\Database\Eloquent\Builder;
-use Q8Intouch\Q8Query\Core\Utils;
-use Q8Intouch\Q8Query\Filterer\Expression;
-use Q8Intouch\Q8Query\Filterer\Filterer;
-
 class DoesntHaveFilterer extends ExistenceMethodBase
 {
 
     public function validate($expression): bool
     {
-        return parent::validate($expression) && $expression->lexemes[0] == 'doesntHave' ;
+        return parent::validate($expression) && $expression->lexemes[0] == 'doesntHave';
     }
 
     protected function getConstrainClosure($expression)
--- src/Filterer/FilterMethods/HasFilterer.php
+++ src/Filterer/FilterMethods/HasFilterer.php
@@ -3,11 +3,6 @@
 namespace Q8Intouch\Q8Query\Filterer\FilterMethods;
 
 
-use Illuminate\Database\Eloquent\Builder;
-use Q8Intouch\Q8Query\Core\Utils;
-use Q8Intouch\Q8Query\Filterer\Expression;
-use Q8Intouch\Q8Query\Filterer\Filterer;
-
 class HasFilterer extends ExistenceMethodBase
 {
 
